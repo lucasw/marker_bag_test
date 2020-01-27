8b1bda2f4939b6b6eb5e1d3d6925e7a35fcf2273
Lucas Walter <wsacul@gmail.com>
2020-01-27 10:40:07 -0800

    So far can't produce any errors in bag read back, may try to load up the system more

--- scripts/bag_read.py
+++ scripts/bag_read.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
 
 import rosbag
+import rospy
 import sys
 
 
-print(sys.argv)
-bag = rosbag.Bag(sys.argv[1])
+rospy.init_node('bag_read')
+bag_name = sys.argv[1]
+
+rospy.loginfo('loading {}'.format(bag_name))
+t0 = rospy.Time.now()
+bag = rosbag.Bag(bag_name)
+t1 = rospy.Time.now()
+dt = (t1 - t0).to_sec()
+rospy.loginfo('{} seconds to load {}'.format(dt, bag_name))
+
 info = bag.get_type_and_topic_info()
-print(info)
+rospy.loginfo(info)
 count = 0
 for topic, msg, stamp in bag.read_messages():
-    if count % 500 == 0:
+    if rospy.is_shutdown():
+        break
+    if count % 300 == 0:
         # for marker in msg.markers:
-        print('{} {} {}'.format(stamp, topic, type(msg)))
+        print('{} {} {} {}'.format(count, stamp, topic, type(msg)))
     count += 1
 bag.close()
--- scripts/marker_array_pub.py
+++ scripts/marker_array_pub.py
@@ -12,11 +12,12 @@ class MarkerArrayPub:
     def __init__(self):
         self.period = rospy.get_param("~period", 0.1)
         self.marker_pub = rospy.Publisher('visualization_marker_array', MarkerArray, queue_size=2)
-        self.timer = rospy.Timer(rospy.Duration(0.05), self.update)
+        self.phase = 0.0
+        self.timer = rospy.Timer(rospy.Duration(self.period), self.update)
 
     def update(self, event):
         msg = MarkerArray()
-        for i in range(16):
+        for i in range(8):
             marker = Marker()
             marker.header.frame_id = 'map'
             marker.header.stamp = event.current_real
@@ -31,17 +32,18 @@ class MarkerArrayPub:
             marker.scale.x = 0.01
             marker.scale.y = 0.0
             marker.scale.z = 0.0
-            for j in range(5000):
+            for j in range(3000):
                 pt = Point()
                 fr = float(j) / 200.0
                 # spiral
                 sc = 0.8
-                pt.x = sc * math.cos(fr * math.pi) + i * 2.
-                pt.y = sc * math.sin(fr * math.pi)
-                pt.z = fr * 0.1
+                pt.x = sc * math.cos(fr * math.pi + self.phase) + i * 2.
+                pt.y = sc * math.sin(fr * math.pi + self.phase)
+                pt.z = fr * 0.15
                 marker.points.append(pt)
             msg.markers.append(marker)
         self.marker_pub.publish(msg)
+        self.phase += self.period * 0.3333
 
 if __name__ == '__main__':
     rospy.init_node('marker_array_pub')
